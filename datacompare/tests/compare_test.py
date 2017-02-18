396a1f100a4185d3637a051e2151bc8773414af7
Marcus Rehm <marcus.rehm@gmail.com>
2017-02-17 23:27:31 -0200

    test update

--- datacompare/tests/compare_test.py
+++ datacompare/tests/compare_test.py
@@ -34,12 +34,12 @@ def test_compare_datasets_with_different_column_names():
     df2 = pd.DataFrame({'Z': ['A0', 'A1', 'A6', 'A7'],
                         'N': ['B0', 'B5', 'B6', 'B7'],
                         'C': ['C0', 'C5', 'C2', 'C7'],
-                        'J': ['D0', 'D5', 'D2', 'D7']})
+                        'W': ['D0', 'D5', 'D2', 'D7']})
 
     df1_keys = ['A']
     df2_keys = ['Z']
     df1_columns_to_compare = ['B', 'D']
-    df2_columns_to_compare = ['N', 'J']
+    df2_columns_to_compare = ['N', 'W']
 
     df_result = cp.compare_datasets(df1, df2, df1_keys, df2_keys,
                                     df1_columns_to_compare,
